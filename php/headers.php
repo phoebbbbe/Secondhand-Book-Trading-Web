b108de2db64467f324371779ef436ff6f5f98a17
phoebbbbe <leelemons1999@gmail.com>
2022-05-05 00:27:15 +0800

    更正

--- php/headers.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=UTF-8');
-header("Access-Control-Allow-Origin: http://localhost:8080");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Cookie');
-header('Access-Control-Allow-Credentials: true');
-?>
